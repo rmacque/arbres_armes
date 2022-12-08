3ceb795031613917186a7d6a1d55546f87166c8a
rmacque <119414194+rmacque@users.noreply.github.com>
2022-12-08 17:04:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -40,24 +40,33 @@ function row_append(table, index, descendant = 1, img = "grandeepee"){
     let cell6 = row.insertCell(5);
     let cell7 = row.insertCell(6);
     let cell8 = row.insertCell(7);
-    for(let i = 0; i< descendant - 1; i++){
-        html += "<span class=\"descendant\"></span>";
+
+    if(row.nextSibling.classList.contains("titre")){
+
     }
+
+    /*
+    for(let i = 0; i < descendant ; i++){
+        html += "<span class=\"descendant-" + (descendant + 1) + "\"></span>";
+    }
+    */
+    html += "<span class=\"descendant-" + descendant + "\"></span>";
     html += "<span class=\"enfant\"></span><img alt=\"ne marche pas\" src=\"images/"+ img +".png\" class=\"grandeepee\"></div><div><button class=\"btn_leger amelioration\">amélioration</button><button class=\"btn_leger supprimer\">supprimer</button></div>";
     cell1.innerHTML = html;
-    cell2.innerHTML = "&Eacute;Name";
-    cell3.innerHTML = "Nan";
+    cell2.innerHTML = "<input placeholder=\"&Eacute;Name\"/>";
+    cell3.innerHTML = "<input placeholder=\"100\"/>";
     cell4.innerHTML = "tranchant";
-    cell5.innerHTML = "---";
-    cell6.innerHTML = "0%";
-    cell7.innerHTML = "---";
-    cell8.innerHTML = "---";
+    cell5.innerHTML = "<input placeholder=\"10\"/>";
+    cell6.innerHTML = "<input placeholder=\"0\">%</input>";
+    cell7.innerHTML = "<input placeholder=\"OOO\"/>";
+    cell8.innerHTML = "<input placeholder=\"15 def\"/>";
 
-    //on attache les fonctions aux boutons
+    /*
     if(!(row.rowIndex % 2)){
         row.classList += "impair" ;
     }
-    console.log(row);
+    */
+    //console.log(row);
     row.getElementsByClassName("supprimer")[0].onclick = function(){
         parentNodeNiv(this,3).remove();
     };
@@ -69,7 +78,14 @@ function row_append(table, index, descendant = 1, img = "grandeepee"){
 function section_create(table, titre, img){
     //1ere et 2eme ligne
     let chaine = "<tr class=\"titre\"><td colspan=\"8\" class=\"nom_arbre\">Arbre "+titre+"</td></tr><tr class=\"caracteristiques\"><td>Rareté et arborescence</td><td>Nom</td><td>Dégâts</td><td>Tranchant</td><td>Attribut</td><td>Affinité</td><td>Fentes</td><td>Bonus</td></tr>";
-    //3eme ligne
-    chaine += "<tr><td><span class=\"origine\"></span><img alt=\"ne marche pas\" src=\"images/"+ img +".png\" class=\"grandeepee\"></div><div><button class=\"btn_leger amelioration\">amélioration</button></div></td><td>&Eacute;Name</td><td>Nan</td><td>Lorem</td><td>---</td><td>0%</td><td>---</td><td>---</td></tr>"
+    //3eme ligne, initialisation
+    chaine += "<tr><td><span class=\"origine\"></span><img alt=\"ne marche pas\" src=\"images/"+ img +".png\" class=\"grandeepee\"></div><div><button class=\"btn_leger amelioration\">amélioration</button></div></td><td>&Eacute;pée "+ titre +"</td><td>Nan</td><td>Lorem</td><td>---</td><td>0%</td><td>---</td><td>---</td></tr>"
     table.innerHTML += chaine
 }
+
+/**
+ * Faire un script pour réaffecter la classe impair à chaque ligne concernée lors des suppressions 
+    if(!(row.rowIndex % 2)){
+        row.classList += "impair" ;
+    }
+*/
